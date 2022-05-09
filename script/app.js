2482276c91c0793ea97ab51f321b0667bd8eca7b
peru3232 <prw@qmail.com.de>
2022-05-09 08:44:16 +0200

    add assignment and updates

---
+++ script/app.js
@@ -0,0 +1,18 @@
+const rec1 = document.getElementById("recipe1");
+const rec2 = document.getElementById("recipe2");
+const rec3 = document.getElementById("recipe3");
+const rec4 = document.getElementById("recipe4");
+const rec5 = document.getElementById("recipe5");
+const rec6 = document.getElementById("recipe6");
+const rec7 = document.getElementById("recipe7");
+const rec8 = document.getElementById("recipe8");
+const rec9 = document.getElementById("recipe9");
+
+
+function selectRecipe(nr){
+
+}
+
+
+
+rec1.addEventListener('click', selectRecipe.bind(this), )
